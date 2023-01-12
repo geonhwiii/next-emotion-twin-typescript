d2a37f9afe246387972c7feebdcb57eadea13323
geonhwiii <geonhwiii@github.com>
2023-01-12 18:36:32 +0900

    add pretter

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
-import type { AppProps } from "next/app";
-import { cache } from "@emotion/css";
-import { CacheProvider } from "@emotion/react";
-import GlobalStyles from "@/styles/GlobalStyles";
+import type { AppProps } from 'next/app';
+import { cache } from '@emotion/css';
+import { CacheProvider } from '@emotion/react';
+import GlobalStyles from '@/styles/GlobalStyles';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
